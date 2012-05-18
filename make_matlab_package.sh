f84d9767e6556613d541715ec555d6768eea1db9
Julien Mairal <julien.mairal@m4x.org>
2012-05-18 19:22:17 +0000

    bug corrction in packaging script

--- make_matlab_package.sh
+++ make_matlab_package.sh
@@ -1,7 +1,7 @@
 rm -rf spams-matlab
-svn export branches/v2.2 spams-matlab
+svn export ./ spams-matlab
 rm -rf spams-matlab/swig/
-rm spams/make_matlab_package.sh
+rm spams-matlab/make_matlab_package.sh
 da=$(``date +%F)
 echo $da
 tar -czf spams-matlab-v2.2-svn$da.tar.gz spams-matlab
